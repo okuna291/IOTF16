b6bf7f7b2be70856b194f0e05afd9b75159dae00
okuna291 <aokunseinde@gmail.com>
2016-09-28 17:59:29 -0400

    ardTopi

---
+++ ardTopi.ino
@@ -0,0 +1,22 @@
+const int ledPin = 12;
+
+void setup(){
+pinMode(ledPin, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop(){
+if (Serial.available()) {
+light(Serial.read() – ‘0’);
+}
+delay(500);
+}
+
+void light(int n){
+for (int i = 0; i < n; i++) {
+digitalWrite(ledPin, HIGH);
+delay(100);
+digitalWrite(ledPin, LOW);
+delay(100);
+}
+}
